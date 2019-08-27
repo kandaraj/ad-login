05790608d66861fa398ccba97507ec8edfed36a9
Siva Kandaraj <skandaraj@agl.com.au>
2019-08-27 16:58:00 +1000

    Active directory login on load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MsAdalAngular6Module } from 'microsoft-adal-angular6';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MsAdalAngular6Module.forRoot({
+      tenant: 'xxxxxx-915d-4d67-aaf9-ce327e8fc59f',
+      clientId: 'xxxxxx-1758-4e5f-a9e2-7aa5384dc655',
+      redirectUri: window.location.origin,
+      endpoints: {
+        'https://localhost/Api/': 'xxx-bae6-4760-b434-xxx'
+      },
+      navigateToLoginRequestUrl: false,
+      cacheLocation: 'localStorage'
+    }),
     BrowserModule,
     AppRoutingModule
   ],
